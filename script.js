6fb52fef94700070fa8b819140feea3287e6e645
MontherJumah <105142971+MontherJumah@users.noreply.github.com>
2025-07-16 08:05:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("clickMe");
+  button.addEventListener("click", function () {
+    alert("أهلاً بك في Nexdo 🎉");
+  });
+});
